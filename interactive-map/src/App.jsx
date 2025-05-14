5e8975ab16f4a7b6357b7bd980a6991775accf69
Vlad <daemondvlad@gmail.com>
2025-05-14 22:18:07 +0500

    Первый коммит: загрузка проекта

---
+++ interactive-map/src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./Map";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps + Autocomplete</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-map-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Map from "./Map";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Интерактивная карта HERE Maps</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
